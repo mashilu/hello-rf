cb3045c0550a2aff371c831e97c468d63935affa
mashilu <mslbuaa@163.com>
2018-03-29 14:15:05 +0800

    ***

--- test/wxPythonTest.py
+++ test/wxPythonTest.py
@@ -3,8 +3,7 @@
 import wx
 
 app = wx.App()
-window = wx.Frame(None, title="第一个GUI应用", size=(300, 200))
-panel = wx.Panel(window)
-label = wx.StaticText(panel, label="Hello world", pos=(100, 50))
-window.Show(True)
+window = wx.Frame(None, -1, "install test")
+btn = wx.Button(window, label="Button")
+window.Show()
 app.MainLoop()
